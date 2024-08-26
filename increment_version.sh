412712a75656d0fb773c944ef508cbab51c9265a
Junaid Malik <86103833+malikjaid@users.noreply.github.com>
2024-08-26 18:40:50 +0530

    Update increment_version.sh

--- increment_version.sh
+++ increment_version.sh
@@ -4,7 +4,7 @@
 git fetch --tags
 
 # Check if an initial version is set via an environment variable or a specific file
-INITIAL_VERSION="4500.0.0"  # Set this to "9000.0.0" only for the first run
+INITIAL_VERSION=""  # Set this to "9000.0.0" only for the first run
 
 # Use the initial version if specified; otherwise, continue from the latest tag
 if [ -n "$INITIAL_VERSION" ]; then
