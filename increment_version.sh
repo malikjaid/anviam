14c78ac9e6acf7b1c10143c9e0e91385547f5200
Junaid Malik <86103833+malikjaid@users.noreply.github.com>
2024-08-26 14:54:25 +0530

    Update increment_version.sh

--- increment_version.sh
+++ increment_version.sh
@@ -7,12 +7,12 @@ git fetch --tags
 CURRENT_BRANCH=$(git branch --show-current)
 
 # Set initial version based on branch
-if [ "$CURRENT_BRANCH" == "malikt" ]; then
+if [ "$CURRENT_BRANCH" == "main" ]; then
     VERSION_FILE="version-main.php"
     INITIAL_VERSION="220.0.0"
-elif [ "$CURRENT_BRANCH" == "main" ]; then
+elif [ "$CURRENT_BRANCH" == "malikt" ]; then
     VERSION_FILE="version-dev.php"
-    INITIAL_VERSION="230.1.0"
+    INITIAL_VERSION="230.1.0-beta"
 else
     VERSION_FILE="version-$CURRENT_BRANCH.php"
     INITIAL_VERSION="1223.0.0-$CURRENT_BRANCH"
