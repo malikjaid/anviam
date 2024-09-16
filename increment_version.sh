23d8ab4cc1b44b7947e55a90355256116af01c0a
Junaid Malik <86103833+malikjaid@users.noreply.github.com>
2024-09-16 11:11:08 +0530

    Update increment_version.sh

--- increment_version.sh
+++ increment_version.sh
@@ -5,7 +5,7 @@ git fetch --tags
 
 
 # Check if an initial version is set via an environment variable or a specific file
-INITIAL_VERSION="0.2.0"         # Set this to "2.0.0" only for the first run
+INITIAL_VERSION=""         # Set this to "2.0.0" only for the first run
 VERSION_FILE="version.php"
 
 # Use the initial version if specified; otherwise, continue from the latest tag
